d5043e15967463b6f8a15ff5c37e603f0c0eda5e
mare5x <mare5x.mare5x@gmail.com>
2018-10-22 22:20:24 +0200

    Added a README file and cleaned up main.cpp (previously SuperHaxagon.cpp).

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-SuperHaxagon
-============
+# SuperHaxagon
 
-[Super Hexagon](http://store.steampowered.com/app/221640/) bot for the Steam (Windows) version. [Video](https://www.dropbox.com/s/8s3cpgc2tp93opp/super_haxagon.mp4)
+## Overview
+
+_**SuperHaxagon**_ is a _DLL injection_ based bot for the game _Super Hexagon_.
+
+_SuperHaxagon_ was built to explore hooking into an _OpenGL_ (_glut_) application
+to provide custom drawing and to modify the game state to the user's liking.
+
+## Features
+
+_SuperHaxagon_ can:
+
+ * play the game using either natural or instant movements,
+ * draw using _OpenGL_ functions,
+ * call _glut_ and _fmodex_ functions directly,
+ * remove window resizing constraints, 
+ * control the game _zoom_ level,
+ * control the game rotation speed,
+ * ...
+
+## Usage
+
+Run with    
+>  > SuperHaxagon.exe _path\_to\_dll_
+
+where _path\_to\_dll_ is the path to _SuperHaxagon.dll_.
+
+Access the bot settings menu in-game by pressing the _middle mouse_ button.
---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include <Windows.h>
+#include <cstdio>
+#include <cassert>
+
+
+HMODULE load_dll(HANDLE proc, const char* dll_path)
+{
+	// write the dll path to process memory 
+	size_t path_len = strlen(dll_path) + 1;
+	LPVOID remote_string_address = VirtualAllocEx(proc, NULL, path_len, MEM_COMMIT, PAGE_EXECUTE);
+	WriteProcessMemory(proc, remote_string_address, dll_path, path_len, NULL);
+
+	// get the address of the LoadLibrary()
+	HMODULE k32 = GetModuleHandleA("kernel32.dll");
+	LPVOID load_library_adr = GetProcAddress(k32, "LoadLibraryA");
+
+	// create the thread
+	HANDLE thread = CreateRemoteThread(proc, NULL, NULL, (LPTHREAD_START_ROUTINE)load_library_adr, remote_string_address, NULL, NULL);
+
+	// finish and clean up
+	WaitForSingleObject(thread, INFINITE);
+
+	DWORD dll_handle;
+	GetExitCodeThread(thread, &dll_handle);
+
+	CloseHandle(thread);
+
+	VirtualFreeEx(proc, remote_string_address, path_len, MEM_RELEASE);
+
+	return (HMODULE)dll_handle;
+}
+
+
+void unload_dll(HANDLE proc, HMODULE dll_handle)
+{
+	// get the address of FreeLibrary()
+	HMODULE k32 = GetModuleHandleA("kernel32.dll");
+	LPVOID free_library_adr = GetProcAddress(k32, "FreeLibrary");
+
+	HANDLE thread = CreateRemoteThread(proc, NULL, NULL, (LPTHREAD_START_ROUTINE)free_library_adr, dll_handle, NULL, NULL);
+
+	WaitForSingleObject(thread, INFINITE);
+
+	DWORD exit_code;
+	GetExitCodeThread(thread, &exit_code);
+
+	CloseHandle(thread);
+}
+
+
+int main(int argc, char** argv, char** env)
+{
+	auto hWnd = FindWindow(nullptr, "Super Hexagon");
+	assert(hWnd);
+
+	DWORD processId = -1;
+	GetWindowThreadProcessId(hWnd, &processId);
+	assert(processId > 0);
+
+	auto const hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
+	assert(hProcess);
+
+	if (argc > 1) {
+		// For LoadLibrary to find the dll path, the dll must either be in the working directory
+		// of the process (SuperHexagon.exe's installation folder) or the path must be absolute.
+		HMODULE dll_handle = load_dll(hProcess, argv[1]);
+		if (dll_handle)
+			printf("%x\n", dll_handle);
+		else
+			printf("Error injecting DLL!\n");
+	}
+
+	CloseHandle(hProcess);
+
+	return 0;
+}
