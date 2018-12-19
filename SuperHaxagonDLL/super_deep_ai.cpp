c83f9b6867a6f41b6733724e8f6eceeb085a0dbe
mare5x <mare5x.mare5x@gmail.com>
2018-12-19 17:33:45 +0100

    Start adding validation replays.

--- SuperHaxagonDLL/super_deep_ai.cpp
+++ SuperHaxagonDLL/super_deep_ai.cpp
@@ -5,6 +5,7 @@
 #include "genann.h"
 #include <cmath>
 #include <deque>
+#include <vector>
 
 
 namespace {
@@ -17,10 +18,10 @@ namespace {
 
 	struct ReplayEntry {
 		GameState state;
-		bool action[3];  // which of the three outputs was chosen as the action to take?
+		int action;  // which action was taken?
 	};
 
-	const size_t ANN_INPUTS = 15;  // size of GameState
+	const size_t ANN_INPUTS = sizeof(GameState) / sizeof(double);
 	const size_t ANN_HIDDEN_LAYERS = 2;
 	const size_t ANN_HIDDEN_SIZE = 10;
 	const size_t ANN_OUTPUTS = 3;
@@ -29,14 +30,17 @@ namespace {
 	const char* ANN_FPATH = "super_weights.ann";
 
 	const size_t HISTORY_SIZE = 18000;  // 5 minutes of 60 frames per second
-	const size_t MEM_BATCH_SIZE = 42;
-	const size_t AI_OBSERVATION_STEPS = 32;  // How many steps to observe before training?
+	const size_t VALIDATION_SIZE = 0.1 * HISTORY_SIZE;  // How many replays should be stored to be used to evaluate the ann's performance?
+	const size_t MEM_BATCH_SIZE = 32;
+	const size_t AI_OBSERVATION_STEPS = 8;  // How many steps to observe before training?
 
 	unsigned long frame_counter = 0;
 
 	genann* ann;
 
 	std::deque<ReplayEntry> replay_memory;
+
+	std::vector<ReplayEntry> validation_replays;
 
 	ReplayEntry* replay_batch[MEM_BATCH_SIZE];
 	double desired_ann_outputs[MEM_BATCH_SIZE][ANN_OUTPUTS];
@@ -47,7 +51,8 @@ void train_ann(ReplayEntry** memory_batch, const double desired_outputs[MEM_BATC
 void get_game_state(SuperStruct* super, GameState* game_state);
 void process_results(ReplayEntry** memory_batch, double desired_outputs[MEM_BATCH_SIZE][ANN_OUTPUTS]);
 void sample_memory(std::deque<ReplayEntry>& memory_batch, ReplayEntry** dst, size_t size);
-void store_replay(GameState& game_state, size_t action_idx, std::deque<ReplayEntry>& replay_memory);
+void store_replay(GameState& game_state, int action, std::deque<ReplayEntry>& replay_memory);
+void evaluate_performance();
 
 
 void _print_arr(const double* arr, int size)
@@ -69,7 +74,7 @@ void _print_mem(const ReplayEntry& mem)
 {
 	printf("state: \n");
 	_print_state(mem.state);
-	printf("action: %d %d %d\n", mem.action[0], mem.action[1], mem.action[2]);
+	printf("action: %d\n", mem.action);
 }
 
 
@@ -125,6 +130,8 @@ void dqn_ai::init(bool load)
 
 	printf("Initialized DQN AI [%d] [%d] [%d] [%d]\n", 
 			ann->inputs, ann->hidden_layers, ann->hidden, ann->outputs);
+
+	validation_replays.reserve(VALIDATION_SIZE);
 }
 
 void dqn_ai::exit(bool save)
@@ -143,10 +150,20 @@ void dqn_ai::report_death(SuperStruct* super)
 	static int train_iteration = 0;
 	static char time_str[32];
 	printf("Training AI [%d] : [%s] ...\n", ++train_iteration, super->get_elapsed_time(time_str));
+	evaluate_performance();
 
 	replay_memory.clear();
+	validation_replays.clear();
 
 	frame_counter = 0;
+}
+
+int get_ann_action(const GameState& game_state)
+{
+	const double* outputs = genann_run(ann, (const double*)&game_state);
+	//_print_arr(outputs, ANN_OUTPUTS);
+	size_t action_idx = arg_max(outputs, ANN_OUTPUTS);
+	return get_action(action_idx);
 }
 
 int dqn_ai::get_move_dir(SuperStruct * super, bool learning)
@@ -169,9 +186,8 @@ int dqn_ai::get_move_dir(SuperStruct * super, bool learning)
 		// Observe the game state and super_ai's action.
 
 		action = get_move_dir(super);
-		size_t action_idx = get_action_idx(action);
 
-		store_replay(game_state, action_idx, replay_memory);
+		store_replay(game_state, action, replay_memory);
 
 		if (frame_counter >= AI_OBSERVATION_STEPS && replay_memory.size() > 2 * MEM_BATCH_SIZE) {
 			frame_counter = 0;
@@ -180,10 +196,7 @@ int dqn_ai::get_move_dir(SuperStruct * super, bool learning)
 			train_ann(replay_batch, desired_ann_outputs, MEM_BATCH_SIZE);
 		}
 	} else {
-		const double* outputs = genann_run(ann, (const double*)&game_state);
-		size_t action_idx = arg_max(outputs, ANN_OUTPUTS);
-		action = get_action(action_idx);
-		_print_arr(outputs, ANN_OUTPUTS);
+		action = get_ann_action(game_state);
 	}
 
 	return action;
@@ -193,7 +206,7 @@ int dqn_ai::get_move_dir(SuperStruct * super, bool learning)
 // To train the ANN, we must feed it the input states and the resulting outputs.
 // The inputs and outputs are observed by playing the game. 
 // The ANN is fed as input GameStates and it outputs the probability of going
-// left, right or nowhere based on the input in three output neurons.
+// left or right (or nowhere) based on the input in the output neurons.
 // All inputs and outputs in the ANN are normalized [0, 1].
 void train_ann(ReplayEntry** memory_batch, const double desired_outputs[MEM_BATCH_SIZE][ANN_OUTPUTS], size_t size)
 {
@@ -260,8 +273,10 @@ void process_results(ReplayEntry** memory_batch, double desired_outputs[MEM_BATC
 {
 	for (int i = 0; i < MEM_BATCH_SIZE; ++i) {
 		const ReplayEntry& mem = *memory_batch[i];
-		for (int j = 0; j < ANN_OUTPUTS; ++j)
-			desired_outputs[i][j] = mem.action[j];
+		size_t action_idx = get_action_idx(mem.action);
+		for (int j = 0; j < ANN_OUTPUTS; ++j) {
+			desired_outputs[i][j] = (action_idx == j) * (mem.action == 0 ? 0.5 : 1);
+		}
 	}
 }
 
@@ -276,25 +291,45 @@ void sample_memory(std::deque<ReplayEntry>& memory_batch, ReplayEntry** dst, siz
 
 	for (int i = size; i < memory_batch.size(); ++i) {
 		int j = rand() % i;
-		// Bias non-neutral movement training.
-		if (j < (memory_batch[i].action[get_action_idx(0)] ? size * 2 / 3 : size)) {
+		if (j < (memory_batch[i].action == 0 ? 2.0 / 3.0 * size : size))
 			dst[j] = &memory_batch[i];
-		}
 	}
 }
 
-void store_replay(GameState& game_state, size_t action_idx, std::deque<ReplayEntry>& replay_memory)
+void store_replay(GameState& game_state, int action, std::deque<ReplayEntry>& replay_memory)
 {
 	ReplayEntry mem;
 	mem.state = std::move(game_state);
-	for (int i = 0; i < ANN_OUTPUTS; ++i)
-		mem.action[i] = (i == action_idx);
+	mem.action = action;
+
+	// Decide whether to store the replay into the replay memory or the validation batch.
+	// (reservoir sampling style)
+	size_t validation_replays_size = max(4, validation_replays.size());
+	if (validation_replays_size < VALIDATION_SIZE) {
+		size_t history_size = replay_memory.size();
+		int k = rand() % (history_size ? history_size : 4);
+		if (k < validation_replays_size) {
+			validation_replays.push_back(mem);
+			return;
+		}
+	}
 
 	replay_memory.push_back(mem);
 
 	// Keep the size of the batch bounded.
 	// Q: Should we keep the most recent history or should it be random?
-	if (replay_memory.size() > HISTORY_SIZE) {
+	if (replay_memory.size() > HISTORY_SIZE)
 		replay_memory.pop_front();
+}
+
+void evaluate_performance()
+{
+	size_t correct_answers = 0;
+	for (const ReplayEntry& mem : validation_replays) {
+		int ann_action = get_ann_action(mem.state);
+		if (ann_action == mem.action)
+			++correct_answers;
 	}
+	size_t incorrect_answers = validation_replays.size() - correct_answers;
+	printf("ANN performance: %d/%d correct [%.2f]\n", correct_answers, incorrect_answers, (double)correct_answers / incorrect_answers);
 }
